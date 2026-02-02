a8f7d0192b159322df4197cc6556ef146e9f270f
annimarie01 <anni.thekindfairy@gmail.com>
2026-02-02 00:17:48 -0700

    emailjs

---
+++ script.js
@@ -0,0 +1,24 @@
+// 
+// 
+// 
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += ' modal__overlay--visible'
+    emailjs
+    .sendForm(
+        'service_hcw6lvq',
+        'template_kpc51m8',
+        event.target,
+        'sha_EqBtAvcUuOzyH'
+    ).then(() => {
+        loading.classList.remove('modal__overlay--visible')
+        success.classList += ' modal__overlay--visible'
+    }).catch(() => {
+        alert(
+            "Sorry, something broke >.< Contact me at anni.thekindfairy@gmail.com"
+        )
+    })
+}
